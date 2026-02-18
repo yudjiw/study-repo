5a235e4af414016eb6ba77c834f657944d3de4f6
Aleksandr <s_khoroshavin@outlook.com>
2026-02-18 23:08:39 +0500

    first commit

---
+++ feature1/feature1.go
@@ -0,0 +1,7 @@
+package feature1
+
+import "fmt"
+
+func Feature1() {
+	fmt.Println("I'm Feature1")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Study/feature1"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Git")
+	feature1.Feature1()
+}
